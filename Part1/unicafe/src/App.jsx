4afb0d71b4e15bd4f065cf87ea0568a43b6c7af1
casingalaaron <casingalaaron04@gmail.com>
2026-07-09 17:39:13 +0800

    Added solution for Exercises 1.11 of Par1d

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -4,15 +4,13 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const hasFeedback = good!==0 || neutral!==0 || bad!==0? true : false
+  const hasFeedback = good!==0 || neutral!==0 || bad!==0
 
   function handleGood(){
     setGood(good + 1)
-    setIsFeedBackEmpty(true)
   }
   function handleNeutral(){
     setNeutral(neutral + 1)
-    setIsFeedBackEmpty(true)
   }
   function handleBad(){
     setBad(bad + 1)
@@ -33,19 +31,32 @@ function App() {
 const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
   const average = (good + (bad * -1)) / total
-  const positive = (good / total) * 100
+  const positive = (good / total) * 100 + " %"
 
   return(
     <div>
       <h1>Statistics</h1>
-      Good:{good}<br/>
-      Neutral: {neutral}<br/>  
-      Bad: {bad}<br/>
-      Total: {total}<br/>
-      Average: {average}<br/>
-      Positives: {positive}%
+      <table>
+        <StatisticLine value={good} text="Good"/>
+        <StatisticLine value={neutral} text="Neutral"/>
+        <StatisticLine value={bad} text="Bad"/>
+        <StatisticLine value={total} text="Total" />
+        <StatisticLine value={average} text="Average"/>
+        <StatisticLine value={positive} text="Positives"/>
+      </table>
     </div>
   )
+}
+
+const StatisticLine = ({text, value}) => {
+  return(<>
+  <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+  </tbody>
+  </>)
 }
 
 export default App
