9501cba00c4421d7ba64be168554de24afdcde76
casingalaaron <casingalaaron04@gmail.com>
2026-07-12 14:18:39 +0800

    Added solution for exercises 2.11 of Part2c

--- Part2/Phonebook/src/App.jsx
+++ Part2/Phonebook/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   
   const [input, setInput] = useState('')
   const handleInput = (event) => {
   setInput(event.target.value)
   }  
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3002/persons')
+    .then(response => {
+      console.log("Promises fulfilled")
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
     <div>{persons.map((item) => console.log(item))}
