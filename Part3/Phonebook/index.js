bbb971e25a31fd1c66a0d4e3b036eca5766cf421
Aaron Casingal <casingalaaron04@gmail.com>
2026-07-17 15:17:12 +0800

    Added and improve solution for Exercises 3.6 of Part3a Phonebook

--- Part3/Phonebook/index.js
+++ Part3/Phonebook/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 
 const PORT = 3001
-
+app.use(express.json())
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
-const data = [
+let data = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -29,9 +29,8 @@ const data = [
       "number": "39-23-6423122"
     }
 ]
-
 app.get('/api/persons', (req, res) => {
-    res.send(data)
+    res.json(data)
 })
 
 app.get('/api/info', (req, res) => {
@@ -52,12 +51,40 @@ app.get('/api/persons/:id', (req, res) => {
 })
 app.delete('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    const person = data.filter(someone => someone.id !== id)
+    const person = data.find(item => item.id === id)
+    
+    if(!person){
+        res.status(404).json(
+            { "error": "Person not found"}
+        )
+    }
+    data = data.filter(person => person.id !== id)
 
-    res.status(200).end()
-    res.send(JSON.stringify(person))
+    res.status(200).json(data)
 })
+
+const generateID = () => {
+    const maxID = data.length > 0 ? Math.max(...data.map(item => Number(item.id))) : 0
+    return String(maxID + 1)
+}
 app.post('/api/persons', (req, res) => {
-    const person = req.body
-    res.JSON(person)
+    const body = req.body
+    const existingName = data.map(item => item.name)
+
+    if(!body || !body.name || !body.number){
+        return res.status(400).json({'error': "name or number is missing"
+        });
+    }
+    else if(existingName.includes(body.name)){
+        return res.status(400).json({"error": "The name already exists in the phonebook"})
+    }
+
+    const person = {
+        'name' : body.name,
+        'number': body.number,
+        'id': generateID()
+    }
+
+    data = data.concat(person)
+    res.json(data)
 })
